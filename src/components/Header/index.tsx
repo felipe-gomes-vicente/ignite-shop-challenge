7ba8be6e43be20c51f21bc6d761143bf3bcbdd2d
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-08-18 10:33:12 -0300

    install lib phosphor-react and @radix-ui/react-dialog, created component Cart and CartButton

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,7 @@
 import Image from "next/image";
+import Link from "next/link";
 
+import { Cart } from "../Cart";
 import logoImg from "../../assets/logo.svg";
 
 import { HeaderContainer } from "./styles";
@@ -7,7 +9,11 @@ import { HeaderContainer } from "./styles";
 export function Header() {
   return (
     <HeaderContainer>
-      <Image width={130} height={52} src={logoImg.src} alt="" />
+      <Link href="/">
+        <Image width={130} height={52} src={logoImg} alt="" />
+      </Link>
+
+      <Cart />
     </HeaderContainer>
   )
 }
