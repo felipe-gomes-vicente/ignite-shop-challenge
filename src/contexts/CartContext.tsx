36837140820e157b40b2e9e9df5a5cb2b8bb7ffd
Felipe Gomes Vicente <felipegomes.vicente2@gmail.com>
2023-10-07 20:56:55 -0300

    feat created CartContext, useCart and CartButton in pages index

---
+++ src/contexts/CartContext.tsx
@@ -0,0 +1,41 @@
+import { createContext, ReactNode, useState } from "react";
+
+export interface IProduct {
+  id: string;
+  name: string;
+  imageUrl: string;
+  price: string;
+  numberPrice: number;
+  description: string;
+  defaultPriceId: string;
+}
+
+interface CartContextData {
+  cartItems: IProduct[];
+  addToCart: (product: IProduct) => void;
+  checkIfItemAlreadyExists: (productId: string) => boolean;
+}
+
+interface CartContextProviderProps {
+  children: ReactNode;
+}
+
+export const CartContext = createContext({} as CartContextData);
+
+export function CartContextProvider({ children }: CartContextProviderProps) {
+  const [cartItems, setCartItems] = useState<IProduct[]>([]);
+
+  function addToCart(product: IProduct) {
+    setCartItems((state) => [...state, product]);
+  }
+
+  function checkIfItemAlreadyExists(productId: string) {
+    return cartItems.some((product) => product.id === productId);
+  }
+
+  return (
+    <CartContext.Provider value={{ cartItems, addToCart, checkIfItemAlreadyExists }}>
+      {children}
+    </CartContext.Provider>
+  )
+}
